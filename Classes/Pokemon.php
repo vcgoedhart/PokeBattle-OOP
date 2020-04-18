9b25531f70e0b341c1e238dea0d1fe35966082f5
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-04-18 22:27:51 +0200

    Added commenting Made class abstract and added abstract method Added getter function

--- Classes/Pokemon.php
+++ Classes/Pokemon.php
@@ -1,5 +1,5 @@
 <?php
-class Pokemon
+abstract class Pokemon
 {
     private $name;
 
@@ -10,11 +10,46 @@ class Pokemon
     protected $resistance;
     protected $weakness;
 
+    /**
+     * Initialiser when an object is constructed.
+     *
+     * @param string $name - Name for the Pokémon.
+     * @param int $maxHealth - Maximum health for the Pokémon to have.
+     */
     public function __construct($name, $maxHealth)
     {
         $this->name = $name;
 
         $this->maxHealth = $maxHealth;
         $this->health = $maxHealth;
+    }
+
+    /**
+     * Abstract function. Every child class requires to have this function.
+     * Must return an array which includes the Attack class inside.
+     *
+     * @return array
+     */
+    abstract public function attacks(): array;
+
+    public function attack(Pokemon $target, Attack $attack)
+    {
+    }
+
+    /**
+     * Gets property values without reading the 'get'
+     * Ex: getName -> name
+     *
+     * @param string $propName - property name. first 3 letters are removed.
+     */
+    public function __get($propName)
+    {
+        $propName = strtolower(substr($propName, 3));
+
+        if (property_exists($this, $propName)) {
+            return $this->$propName;
+        } else {
+            echo "Property '$propName' does not exist.";
+        }
     }
 }
