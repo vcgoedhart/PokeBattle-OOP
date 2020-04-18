7357cfd71a11d91cb5ea034fffec4e1237c1b5cc
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-04-18 21:57:11 +0200

    Created Attack.php

---
+++ Classes/Attack.php
@@ -0,0 +1,12 @@
+<?php
+class Attack
+{
+    public $name;
+    public $damage;
+
+    public function __construct($name, $damage)
+    {
+        $this->name = $name;
+        $this->damage = $damage;
+    }
+}
