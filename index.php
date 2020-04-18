3381f2d7ec4821a0d6a5594ccd05e5a11021d7d3
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-04-18 21:57:05 +0200

    Created index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require "autoload.php";
+
+$pikachu = new Pikachu("Bob", 60);
+$charmeleon = new Charmeleon("Harry", 60);
+
+var_dump($pikachu, $charmeleon);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
