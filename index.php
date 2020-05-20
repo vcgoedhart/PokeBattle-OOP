9b78cf7a75904436d38a5714b1222830eb2ccc38
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-05-20 15:20:05 +0200

    Cleaned up front-end text

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
 require "autoload.php";
 
-// instantiation of objects
+// Instantiation of objects
 $pikachu = new Pikachu("Elektrische Bob", 60);
 $charmeleon = new Charmeleon("Vuur Harry", 60);
 
 // ------------- Epic Pokémon Battle Begins  ------------- //
 
+echo "<b>Status:</b> </br>";
+echo "$pikachu->name's HP: $pikachu->health / $pikachu->maxHealth </br>";
+echo "$charmeleon->name's HP: $charmeleon->health / $charmeleon->maxHealth </br>";
+echo "<s>--------------------------------------</s></br>";
+echo "</br>";
+
 $pikachu->attack($charmeleon, $pikachu->attacks()["Electric Ring"]);
 
-echo "$pikachu->name attacked $charmeleon->name using Electring Ring. </br>";
+echo "$pikachu->name attacked $charmeleon->name using Electric Ring. </br>";
 echo "</br>";
 
 echo "<b>Status Update:</b> </br>";
@@ -30,6 +36,8 @@ echo "<s>--------------------------------------</s></br>";
 echo "</br>";
 
 // ------------- Pokémon Battle Ends ------------- //
+
+echo "<b>Living Pokémon:</b> " . Pokemon::getPopulation();
 
 ?>
 
